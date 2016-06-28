ba117c11c6a8ca72350aa7472621191da70092bd
Will Schumacher <meintodengel@gmail.com>
2016-06-28 14:30:00 -0700

    add CORS support

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS, cross_origin
 
 from resources.todo import Todo
 from resources.todoList import TodoList
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(Todo, '/<todo_id>')
