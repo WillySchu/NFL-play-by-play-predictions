0a00f46710c5f2cb266e97f09a95984d8d460c4f
Will Schumacher <meintodengel@gmail.com>
2016-06-29 14:33:51 -0700

    add random forest predictor

--- server/common/predict.py
+++ server/common/predict.py
@@ -4,13 +4,50 @@ import itertools
 import numpy as np
 from sklearn import tree
 from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
 
 def loadData(data='nflplaybyplay2015.csv', low_memory = False):
     return pd.read_csv(data)
 
 def formatData(data):
 
+    teams = {
+        'ARI': 0,
+        'ATL': 1,
+        'BAL': 2,
+        'BUF': 3,
+        'CAR': 4,
+        'CHI': 5,
+        'CIN': 6,
+        'CLE': 7,
+        'DAL': 8,
+        'DEN': 9,
+        'DET': 10,
+        'GB': 11,
+        'HOU': 12,
+        'IND': 13,
+        'JAX': 14,
+        'KC': 15,
+        'MIA': 16,
+        'MIN': 17,
+        'NE': 18,
+        'NO': 19,
+        'NYG': 20,
+        'NYJ': 21,
+        'OAK': 22,
+        'PHI': 23,
+        'PIT': 24,
+        'SD': 25,
+        'SEA': 26,
+        'SF': 27,
+        'STL': 28,
+        'TB': 29,
+        'TEN': 30,
+        'WAS': 31
+    }
+
     data = data[data['PlayType'] != 'Timeout']
+    data = data[data['PlayType'] != 'Two Minute Warning']
 
     def week(date):
         startDate = '2015-09-10'
@@ -29,7 +66,7 @@ def formatData(data):
     data['ScoreDiff'][np.isnan(data['ScoreDiff']) == True] = 0
     data['TimeSecs'][np.isnan(data['TimeSecs']) == True] = 0
 
-    train = data[data['week'] != 12]
+    train = data[data['week'] != 11]
     test = data[data['week'] == 11]
 
     return (train, test)
@@ -52,13 +89,42 @@ def testTree(test, tree=None):
     features = test[['down', 'ydstogo', 'ScoreDiff', 'TimeSecs']].values
     return tree.score(features, target)
 
-def predict(features, tree=None):
+def predictTree(features, tree=None):
     if tree is None:
         tree = joblib.load('common/my_tree.pkl')
     return tree.predict(features)
 
-def go():
+def goTree():
     data = loadData()
     train, test = formatData(data)
     my_tree = makeTree(train)
     return testTree(test, my_tree)
+
+def makeForest(train):
+    target = train['passed'].values
+    features = train[['down', 'ydstogo', 'ScoreDiff', 'TimeSecs']].values
+
+    my_forest = RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 100, random_state = 1)
+    my_forest = my_forest.fit(features, target)
+
+    joblib.dump(my_forest, 'my_forest.pkl')
+
+    return my_forest
+
+def testForest(test, forest=None):
+    if forest is None:
+        forest = joblib.load('my_forest.pkl')
+    target = test['passed'].values
+    features = test[['down', 'ydstogo', 'ScoreDiff', 'TimeSecs']].values
+    return forest.score(features, target)
+
+def predictForest(features, forest=None):
+    if forest is None:
+        forest = joblib.load('my_forest.pkl')
+    return forest.predict(features)
+
+def goForest():
+    data = loadData()
+    train, test = formatData(data)
+    my_forest = makeForest(train)
+    return testForest(test, my_forest)
