7f6a05ab392916e8e521b7ce9ae8fd4d0e965c0d
Will Schumacher <meintodengel@gmail.com>
2016-06-29 12:14:14 -0700

    front end form makes post to back end and gets prediction response

--- server/resources/predict.py
+++ server/resources/predict.py
@@ -4,7 +4,7 @@ from common.predict import predict
 
 parser = reqparse.RequestParser()
 parser.add_argument('down')
-parser.add_argument('yrdstogo')
+parser.add_argument('ydstogo')
 parser.add_argument('ScoreDiff')
 
 class Predict(Resource):
@@ -12,7 +12,7 @@ class Predict(Resource):
         args = parser.parse_args()
         print(args)
         print(args['down'])
-        print(args['yrdstogo'])
+        print(args['ydstogo'])
         print(args['ScoreDiff'])
-        features = [[args['down'], args['yrdstogo'], args['ScoreDiff']]]
+        features = [[args['down'], args['ydstogo'], args['ScoreDiff']]]
         return predict(features)[0]
