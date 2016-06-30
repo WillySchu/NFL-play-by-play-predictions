3a9de7800b7c9305005abe6fe68da7709b5b2186
Will Schumacher <meintodengel@gmail.com>
2016-06-30 15:21:08 -0700

    add extra features to endpoint

--- resources/predict.py
+++ resources/predict.py
@@ -8,13 +8,14 @@ import json
 parser = reqparse.RequestParser()
 parser.add_argument('down')
 parser.add_argument('ydstogo')
+parser.add_argument('yrdline100')
 parser.add_argument('ScoreDiff')
 parser.add_argument('TimeSecs')
 
 class Predict(Resource):
     def post(self):
         args = parser.parse_args()
-        features = [[args['down'], args['ydstogo'], args['ScoreDiff'], args['TimeSecs']]]
+        features = [[args['down'], args['ydstogo'], args['yrdline100'], args['ScoreDiff'], args['TimeSecs']]]
         prediction = predictForest(features)[0]
         return json.dumps(prediction.tolist())
 
