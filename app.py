6fb548057c4f037bb752964cec83f8d8875bf889
Will Schumacher <meintodengel@gmail.com>
2016-06-30 13:16:15 -0700

    config env test

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ from flask_cors import CORS, cross_origin
 from resources.todo import Todo
 from resources.predict import Predict
 
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 CORS(app)
 api = Api(app)
 
 api.add_resource(Todo, '/<todo_id>')
 api.add_resource(Predict, '/')
 
+print(os.environ['APP_SETTINGS'])
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
