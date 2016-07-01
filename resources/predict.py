b97619968bda3e535655b373e0b34ba43a500f5a
Will Schumacher <meintodengel@gmail.com>
2016-07-01 13:00:18 -0700

    edited end point to account for extra feature

--- resources/predict.py
+++ resources/predict.py
@@ -6,6 +6,7 @@ from common.predict import predictForest
 import json
 
 parser = reqparse.RequestParser()
+parser.add_argument('posteamint')
 parser.add_argument('down')
 parser.add_argument('ydstogo')
 parser.add_argument('yrdline100')
@@ -15,7 +16,7 @@ parser.add_argument('TimeSecs')
 class Predict(Resource):
     def post(self):
         args = parser.parse_args()
-        features = [[args['down'], args['ydstogo'], args['yrdline100'], args['ScoreDiff'], args['TimeSecs']]]
+        features = [[args['posteamint'], args['down'], args['ydstogo'], args['yrdline100'], args['ScoreDiff'], args['TimeSecs']]]
         prediction = predictForest(features)[0]
         return json.dumps(prediction.tolist())
 
