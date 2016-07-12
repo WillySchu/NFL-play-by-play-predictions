70d551fe8935098e0081195c67c536fe66983f12
Will Schumacher <meintodengel@gmail.com>
2016-07-12 14:50:48 -0700

    remove debug prints

--- resources/auth.py
+++ resources/auth.py
@@ -52,9 +52,7 @@ class Logout(Resource):
 class Status(Resource):
     def post(self):
         args = parser.parse_args()
-        print(args)
         user = User.verify_auth_token(args['token'])
-        print(user)
         return jsonify({'id': user.id, 'email': user.email, 'favteam': user.favteam})
 
 class UpdateTeam(Resource):
